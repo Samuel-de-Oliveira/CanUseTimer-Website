1efea4fb9783bb53d931c5e891731aa5bb98e43c
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-09-26 14:37:17 -0300

    Add JavaScript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function animation(id) {
+	var main = window.document.getElementById(id)
+	main.style = 'padding: 6px; background: white; color: black;'
+}
+function exitMain(id) {
+	var main = window.document.getElementById(id)
+	main.style = 'padding: 6px; background: #333333; color: white;'
+}
+function exitOuther(id) {
+	var main = window.document.getElementById(id)
+	main.style = 'padding: 3px; background: #444444; color: white;'
+}
